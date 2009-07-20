f2a47ccc5e437e9fe46608792a9760d877a3b411
Wesley Mason <wes@1stvamp.org>
2009-07-20 09:17:44 +0100

    Tweaked tweetback module with docs and new user arg

--- tweetbackweb/lib/tweetback.py
+++ tweetbackweb/lib/tweetback.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 import twitter2
 
-class tweetback(object):
+class Tweetback(object):
 	"""Tweetback
-Class for comparing twitter followers and following
-to see who you're not following who you have had conversations
-with or have just @-replied you.
-"""
-	def __init__(self):
-		"Tweetback constructor"
+	Class for comparing twitter followers and following
+	to see who you're not following who you have had conversations
+	with or have just @-replied you.
+	"""
+	def __init__(self, user=None):
+		"Instantiate a Tweetback object"
+		self.user = user
 
 def main():
-	tb = tweetback()
+	tb = Tweetback()
 	print "tweetback loaded"
 	return
 
