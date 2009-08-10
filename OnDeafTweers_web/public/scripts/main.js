d6d69d513b5d9254f1cff6c23ae593335454f5fa
Wesley Mason <wes@1stvamp.org>
2009-08-10 16:03:10 +0100

    Working on ajax callback, can't quite get the hang of jquery's DOM stuff, seems more limited than prototype

--- OnDeafTweers_web/public/scripts/main.js
+++ OnDeafTweers_web/public/scripts/main.js
@@ -1,6 +1,15 @@
+function update_status() {
+}
 $(document).ready(function() {
 	$("#odt-form").submit(function() {
-		$.get("/tweets/generate_report/" + $("#username").value, function(report) {});
+		$.get("/tweets/generate_report/" + $("#username").val(), function(report) {
+			report_element = $("#report");
+			report_element.empty();
+			report.each(function(i) {
+				li = document.createElement('li');
+			}
+			report_element.slideDown();
+		});
 	});
 });
 
